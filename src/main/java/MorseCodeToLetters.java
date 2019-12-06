bdd4b8a7cf7418f5a54f203dc96c03fd99862643
aldrinbgama <aldrin.gama@umuzi.org>
2019-12-06 12:31:43 +0200

    2nd commit

--- src/main/java/MorseCodeToLetters.java
+++ src/main/java/MorseCodeToLetters.java
@@ -1,12 +1,100 @@
 import java.util.Scanner;
 
 public class MorseCodeToLetters {
-    public void morseToLetters () {
+    public static String decode (String toEncode) {
+        String morse = toEncode;
+
+        if (toEncode.equalsIgnoreCase(".-"))
+            morse = "a";
+        if (toEncode.equalsIgnoreCase("-..."))
+            morse = "b";
+        if (toEncode.equalsIgnoreCase("-.-."))
+            morse = "c";
+        if (toEncode.equalsIgnoreCase("-.."))
+            morse = "d";
+        if (toEncode.equalsIgnoreCase("."))
+            morse = "e";
+        if (toEncode.equalsIgnoreCase("..-."))
+            morse = "f";
+        if (toEncode.equalsIgnoreCase("--."))
+            morse = "g";
+        if (toEncode.equalsIgnoreCase("...."))
+            morse = "h";
+        if (toEncode.equalsIgnoreCase(".."))
+            morse = "i";
+        if (toEncode.equalsIgnoreCase(".---"))
+            morse = "j";
+        if (toEncode.equalsIgnoreCase("-.-"))
+            morse = "k";
+        if (toEncode.equalsIgnoreCase(".-.."))
+            morse = "l";
+        if (toEncode.equalsIgnoreCase("--"))
+            morse = "m";
+        if (toEncode.equalsIgnoreCase("-."))
+            morse = "n";
+        if (toEncode.equalsIgnoreCase("---"))
+            morse = "o";
+        if (toEncode.equalsIgnoreCase(".--."))
+            morse = "p";
+        if (toEncode.equalsIgnoreCase("--.-"))
+            morse = "q";
+        if (toEncode.equalsIgnoreCase(".-."))
+            morse = "r";
+        if (toEncode.equalsIgnoreCase("..."))
+            morse = "s";
+        if (toEncode.equalsIgnoreCase("-"))
+            morse = "t";
+        if (toEncode.equalsIgnoreCase("..-"))
+            morse = "u";
+        if (toEncode.equalsIgnoreCase("...-"))
+            morse = "v";
+        if (toEncode.equalsIgnoreCase(".--"))
+            morse = "w";
+        if (toEncode.equalsIgnoreCase("-..-"))
+            morse = "x";
+        if (toEncode.equalsIgnoreCase("-.--"))
+            morse = "y";
+        if (toEncode.equalsIgnoreCase("--.."))
+            morse = "z";
+        if (toEncode.equalsIgnoreCase("-----"))
+            morse = "0";
+        if (toEncode.equalsIgnoreCase(".----"))
+            morse = "1";
+        if (toEncode.equalsIgnoreCase("..---"))
+            morse = "2";
+        if (toEncode.equalsIgnoreCase("...--"))
+            morse = "3";
+        if (toEncode.equalsIgnoreCase("....-"))
+            morse = "4";
+        if (toEncode.equalsIgnoreCase("....."))
+            morse = "5";
+        if (toEncode.equalsIgnoreCase("-...."))
+            morse = "6";
+        if (toEncode.equalsIgnoreCase("--..."))
+            morse = "7";
+        if (toEncode.equalsIgnoreCase("---.."))
+            morse = "8";
+        if (toEncode.equalsIgnoreCase("----."))
+            morse = "9";
+        if (toEncode.equalsIgnoreCase(" "))
+            morse = "/";
+
+        return morse;
+    }
+    public void morseToLetters() {
+
+        System.out.println(" This is a Morse to Letters Code Translator.  ");
+        System.out.println(" Please enter the morse you would like translate ");
+        System.out.println("             into English Code. ");
+        System.out.println(" ============================================ ");
+
         Scanner input = new Scanner(System.in);
+        String text = input.nextLine();
         String newEnglish = ""; //sets string for newEnglish
         String selectedEnglish; //sets string for selectedEnglish
         String convertedEnglish; //sets string for convertedEnglish
         String[] morseChars = text.split(" ");
+
         for (int i = 0; i < morseChars.length; i++)
         {
             //Select the next Morse character
@@ -22,6 +110,7 @@ public class MorseCodeToLetters {
                 newEnglish = newEnglish + " ";
             }
         }
-        return newEnglish;
+        System.out.print(newEnglish);
+        System.out.println();
     }
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,29 @@
-public class main {
+import java.util.Scanner;
+
+class main {
     public static void main(String[] args) {
-        LettersToMorseCode letter = new LettersToMorseCode();
-        letter.lettersToMorse();
+
+        Scanner input = new Scanner(System.in);
+        Scanner run = new Scanner(System.in);
+        String rerun;
+        do {
+            System.out.println("This program converts English to Morse code and visa versa,");
+            System.out.println("   please press 1 to convert English to morse code and");
+            System.out.println("          2 to convert Morse code to English.");
+            int choice = input.nextInt();
+
+            switch (choice) {
+                case 1:
+                    LettersToMorseCode letter = new LettersToMorseCode();
+                    letter.lettersToMorse();
+                    break;
+                case 2:
+                    MorseCodeToLetters morse = new MorseCodeToLetters();
+                    morse.morseToLetters();
+                    break;
+            }
+            System.out.println("Type 'yes' to run again or any key to exit");
+            rerun = run.nextLine();
+        }while (rerun.equalsIgnoreCase("Yes"));
     }
 }
